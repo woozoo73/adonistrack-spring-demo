5b5f8c9c41127ece4980d39edd58f3401be92534
woozoo73 <woozoo73@gmail.com>
2021-06-12 14:05:08 +0900

    initial commit

---
+++ acu/src/main/java/com/example/acu/AcuApplication.java
@@ -0,0 +1,13 @@
+package com.example.acu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcuApplication.class, args);
+	}
+
+}
---
+++ acu/src/test/java/com/example/acu/AcuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
